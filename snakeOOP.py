ef1d2a4086d6ece4d449cef36750ff680c8d6227
Stastra <stasto44@gmail.comgit config --global user.name Stastragit pushgit config --global user.email stasto44@gmail.com>
2021-09-26 22:17:46 +0300

    added counter, completed the snake

--- snakeOOP.py
+++ snakeOOP.py
@@ -12,6 +12,9 @@ height = 600
 display = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Snake')
 
+#шрифт и размер 
+font = pygame.font.SysFont("None", 35)
+
 #Цвета экрана, змейки, сообщения
 green = (0, 255, 0)
 black = (0, 0, 0)
@@ -20,7 +23,7 @@ red = (213, 50, 80)
 #Начальный размер змейки
 segment_size = 20
 #Скорость движения змейки
-snake_speed = 8
+snake_speed = 5
 
 #Расположение змейки по середине
 head_x = width // 2 // segment_size * segment_size
@@ -32,8 +35,25 @@ def get_random_point():
     y = random.randint(0, height - segment_size) // segment_size * segment_size
     return x, y
 
+#отображаем список змейки, т.е. змейку с новой длинной
+def show_snake(snake):
+    for x in snake:
+        pygame.draw.rect(display, black, [x[0], x[1], segment_size, segment_size])
+        
+#создали счетчик съеденых фруктов
+def show_score(score):
+    value = font.render("Очки: " + str(score), True, black)
+    display.blit(value, [0, 0])
+
 #вызываем метод с координатами рандомной точки
 food_x, food_y = get_random_point()
+
+
+#создаем пустой список
+snake = []
+#реальная длина змейки
+snake_length = 1
+
 
 #Переменные скорость
 vx = 0
@@ -46,8 +66,6 @@ while True:
     
     #если змейка выходит за пределы экрана
     if head_x < 0 or head_x > width - segment_size or head_y < 0 or head_y > height - segment_size:
-        #использовать шрифт и размер для
-        font = pygame.font.SysFont("None", 35)
         #сообщения о проиграше
         message = font.render("Вы проиграли!", True, red) #msg, True, color
         #вывести сообщение в конкретном месте
@@ -81,6 +99,7 @@ while True:
                 vy = 0
                 vx = segment_size 
     
+    #задали движение змейке
     head_x += vx
     head_y += vy
     
@@ -90,13 +109,24 @@ while True:
     #отобразить food квадратом на экране с красным цветом
     pygame.draw.rect(display, red, [food_x, food_y, segment_size, segment_size])
     
-    #отобразить змейку квадратом на экране в черном цвете
-    pygame.draw.rect(display, black, [head_x, head_y, segment_size, segment_size])
+    #отобразить змейку, если фрукт не съеден, то удаляем хвост, оставляем прежний размер
+    snake.append((head_x, head_y))
+    if len(snake) > snake_length:
+        del snake[0]
+        
+    #отобразить змейку и очки
+    show_snake(snake)
+    show_score(snake_length - 1)
+    
+    #отобразить голову змейки квадратом на экране в черном цвете
+    #pygame.draw.rect(display, black, [head_x, head_y, segment_size, segment_size])
     
     #проверка съела ли змейка еду, совпадение координат
     #если съела, то выдаем новую еду с новыми рандомными координатами
+    #и увеличиваем длину змейки на единицу, обновляем и выдаем новую длину змейки
     if head_x == food_x and head_y == food_y:
         food_x, food_y = get_random_point()
+        snake_length += 1
         
     
     #Выполнить команды, вывести на дисплее 
